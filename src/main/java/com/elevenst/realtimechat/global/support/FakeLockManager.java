8a1a781cf18e73e87c588270d51b0e097f3a3e7f
tjdgns0618 <gruddufs@gmail.com>
2026-06-25 20:26:56 +0900

    Limit fake lock manager to test profile

--- src/main/java/com/elevenst/realtimechat/global/support/FakeLockManager.java
+++ src/main/java/com/elevenst/realtimechat/global/support/FakeLockManager.java
@@ -1,9 +1,11 @@
 package com.elevenst.realtimechat.global.support;
 
 import java.util.concurrent.TimeUnit;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("test")
 public class FakeLockManager implements LockManager {
     // 다른 Issue 구현 완료 후 테스트/로컬 대체가 필요 없으면 실제 구현체(RedissonLockManager)로 완전히 교체 예정
     @Override
