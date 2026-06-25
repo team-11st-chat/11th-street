f70f7941dc5d4de531e70b958ad314e540b37435
jy030506@naver.com <jy030506@naver.com>
2026-06-25 11:32:07 +0900

    fix: STOMP 엔드포인트 Origin 허용 설정 추가

--- src/main/java/com/elevenst/realtimechat/global/websocket/WebSocketConfig.java
+++ src/main/java/com/elevenst/realtimechat/global/websocket/WebSocketConfig.java
@@ -17,7 +17,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws");
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("*");
     }
 
     @Override
