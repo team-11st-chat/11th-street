dc6fccd8020df7b87d7dcab4b357c09bfa03856c
WooLamLee <98564214+w00lam@users.noreply.github.com>
2026-06-30 12:04:55 +0900

    refactor: 에러코드 한글화

--- src/main/java/com/elevenst/realtimechat/domain/category/exception/CategoryErrorCode.java
+++ src/main/java/com/elevenst/realtimechat/domain/category/exception/CategoryErrorCode.java
@@ -8,8 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum CategoryErrorCode implements ErrorCode {
-    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "Category not found."),
-    INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "Category information is invalid.");
+    CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "카테고리를 찾을 수 없습니다.."),
+    INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "카테고리 정보가 올바르지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
